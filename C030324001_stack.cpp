fe90ad652436fba2a611d2560617fea8b62eded5
Awi-wijaya <rathomiafdhai@gmail.com>
2025-06-01 21:38:03 +0700

    deklarasi function stack

--- C030324001_stack.cpp
+++ C030324001_stack.cpp
@@ -12,6 +12,11 @@ typedef struct stack
     int Count;
 } Stack;
 
+void InitializeStack(Stack *S);Add commentMore actions
+int Full(Stack *S);
+int Empty(Stack *S);
+void Pop(Stack *S, itemType *x);
+void Push(Stack *S, itemType x);
 int main(int argc, char const *argv[])
 {
     cout << "Hello World!" << endl;
